fb007976b8754c7d34961c4fad8825b196682574
balessan <benoit.alessandroni@gmail.com>
2013-05-02 17:53:01 -0400

    Index page modification

--- index.php
+++ index.php
@@ -35,6 +35,56 @@
 			</div>	
 		</div>
 	</div>
+	<hr class="featurette-divider" />
+	<div class="featurette" height="512" style="min-height:512px; display:block;">
+		<img class="featurette-image pull-right" src="./includes/img/index_didapi.jpg" height="512" width="512"/>
+		<h2 class="featurette-heading">
+			First featurette Heading 
+			<span class="muted">
+				Blablabla
+			</span>
+		</h2>
+		<p class="lead">
+			Donec ullamcorper nulla non metus auctor fringilla. 
+			Vestibulum id ligula porta felis euismod semper. 
+			Praesent commodo cursus magna, vel scelerisque nisl consectetur. 
+			Fusce dapibus, tellus ac cursus commodo.
+		</p>
+	</div>
+	
+	<hr class="featurette-divider" />
+	<div class="featurette" height="512" style="min-height:512px; display:block;">
+		<img class="featurette-image pull-left" src="./includes/img/menu_main.jpg" height="512" width="512"/>
+		<h2 class="featurette-heading">
+			Second featurette Heading 
+			<span class="muted">
+				Blablabla
+			</span>
+		</h2>
+		<p class="lead">
+			Donec ullamcorper nulla non metus auctor fringilla. 
+			Vestibulum id ligula porta felis euismod semper. 
+			Praesent commodo cursus magna, vel scelerisque nisl consectetur. 
+			Fusce dapibus, tellus ac cursus commodo.
+		</p>
+	</div>
+	
+	<hr class="featurette-divider" />
+	<div class="featurette" height="512" style="min-height:512px; display:block;">
+		<img class="featurette-image pull-right" src="./includes/img/menu_onions.jpg" height="512" width="512"/>
+		<h2 class="featurette-heading">
+			Third featurette Heading 
+			<span class="muted">
+				Blablabla
+			</span>
+		</h2>
+		<p class="lead">
+			Donec ullamcorper nulla non metus auctor fringilla. 
+			Vestibulum id ligula porta felis euismod semper. 
+			Praesent commodo cursus magna, vel scelerisque nisl consectetur. 
+			Fusce dapibus, tellus ac cursus commodo.
+		</p>
+	</div>
 </div>
 
 <?php include("footer.php") ?>
